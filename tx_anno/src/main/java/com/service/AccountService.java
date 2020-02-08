a9361652031463a6419745f7a1710f176162f611
lihaobo_glb <18392532787@163.com>
2020-02-08 23:14:22 +0800

    spring的事务控制通过注解

---
+++ tx_anno/src/main/java/com/service/AccountService.java
@@ -0,0 +1,24 @@
+package com.service;
+
+import com.domain.Account;
+
+/**
+ * @outhor li
+ * @create 2020-02-08 8:48
+ */
+public interface AccountService {
+    /**
+     * 根据id查询账户信息
+     * @param accountId
+     * @return
+     */
+    Account findAccountById(Integer accountId);
+
+    /**
+     * 转账
+     * @param sourceName    转成账户名称
+     * @param targetName    转入账户名称
+     * @param money         转账金额
+     */
+    void transfer(String sourceName, String targetName, Float money);
+}
---
+++ tx_anno/src/main/java/com/service/impl/AccountServiceImpl.java
@@ -0,0 +1,38 @@
+package com.service.impl;
+
+import com.dao.AccountDao;
+import com.domain.Account;
+import com.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @outhor li
+ * @create 2020-02-08 8:49
+ */
+@Service("accountService")
+@Transactional(propagation = Propagation.SUPPORTS,readOnly = true)//只读型事务的配置
+public class AccountServiceImpl implements AccountService {
+    @Autowired
+    private AccountDao accountDao;
+
+    public Account findAccountById(Integer accountId) {
+        return accountDao.findAccountById(accountId);
+    }
+
+    //此方法需要的是读写型事务配置
+    @Transactional(propagation = Propagation.REQUIRED,readOnly = false)
+    public void transfer(String sourceName, String targetName, Float money) {
+        System.out.println("transfer");
+        Account source = accountDao.findAccountByName(sourceName);
+        Account target = accountDao.findAccountByName(targetName);
+        source.setMoney(source.getMoney() - money);
+        target.setMoney(target.getMoney() + money);
+        accountDao.updateAccount(source);
+//        int i = 1/0;
+        accountDao.updateAccount(target);
+
+    }
+}
